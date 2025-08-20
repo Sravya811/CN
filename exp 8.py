6c43f9bf744aa5f9718a7b584501c192b61158e5
Sravya bandi <sravyabandi811@gmail.com>
2025-08-20 10:15:30 +0530

    Update  8Q.py

--- exp 8.py
+++ exp 8.py
@@ -14,6 +14,41 @@ def stop_and_wait(frames):
 
     print("\nAll frames sent and acknowledged (Stop-and-Wait simulation complete).")
 
-# Example test
+
 frames = ['A', 'B', 'C', 'D']
 stop_and_wait(frames)
+
+
+
+
+output
+
+Sent Frame 0: A
+Is ACK 0 received? (y/n): y
+ACK 0 received. Moving to next frame.
+
+Sent Frame 1: B
+Is ACK 1 received? (y/n): n
+ACK 1 not received or frame lost. Retransmitting Frame 1.
+
+Sent Frame 1: B
+Is ACK 1 received? (y/n): y
+ACK 1 received. Moving to next frame.
+
+Sent Frame 2: C
+Is ACK 2 received? (y/n): n
+ACK 2 not received or frame lost. Retransmitting Frame 2.
+
+Sent Frame 2: C
+Is ACK 2 received? (y/n): n
+ACK 2 not received or frame lost. Retransmitting Frame 2.
+
+Sent Frame 2: C
+Is ACK 2 received? (y/n): y
+ACK 2 received. Moving to next frame.
+
+Sent Frame 3: D
+Is ACK 3 received? (y/n): y
+ACK 3 received. Moving to next frame.
+
+All frames sent and acknowledged (Stop-and-Wait simulation complete).
